4b8bd44c1fe905a6b94630fdd257f3bbf3a93366
Steve <s.bazant@seznam.cz>
2021-10-07 15:39:59 +0200

    IntelliJ

--- buy_computer.py
+++ buy_computer.py
@@ -4,25 +4,30 @@ choosable_computer_parts = ["computer",
                             "monitor",
                             "keyboard",
                             "mouse",
-                            "mouse mat"
+                            "mouse mat",
+                            "hdmi Cable"
                             ]
 
 chosen_computer_parts = []
 
-while current_choice != 665:
+while current_choice != 0:
     i = 0
     if current_choice in range(0,len(choosable_computer_parts)):
-        print("adding {}".format(current_choice + 1))
+        print("adding {}".format(current_choice))
+        # ind = int(current_choice) - 1
+        # chosen_computer_parts = choosable_computer_parts[ind]
         chosen_computer_parts.append(choosable_computer_parts[current_choice])
         print(chosen_computer_parts)
-        current_choice = int(input()) - 1
     else:
         print("add options from list below")
-        for itter in choosable_computer_parts:
+#        for itter in choosable_computer_parts:
+        for index_positon, object_in_list in enumerate(choosable_computer_parts):
             i += 1
-            print(f"{i}.\t" + itter)
+#            print(f"{i}.\t" + itter)
+#            print("{0}. {1}".format(choosable_computer_parts.index(itter) + 1, itter))
+            print("{0}. {1}". format(index_positon + 1, object_in_list))
         print("666.for exit")
-        current_choice = int(input()) - 1
+    current_choice = int(input())
 
 print("shopping over")
-
+print(chosen_computer_parts)
---
+++ enumerate_example.py
@@ -0,0 +1,36 @@
+# for index, character in enumerate("abcdefgh"):
+#     print(index, character)
+
+
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flowers = []
+shrubs = []
+
+for i in range(0, len(data)):
+    if "Flower" in data[i]:
+        flowers.append(data[i])
+    else:
+        shrubs.append((data[i]))
+print(shrubs)
+print(flowers)
