83e461f7051537c66fee87d30edfde8abbb2d2ee
Steve <s.bazant@seznam.cz>
2021-10-11 15:24:55 +0200

    IntelliJ

--- number_list.py
+++ number_list.py
@@ -1,12 +1,19 @@
 even = [2,4,6,8]
 odd = [1,3,5,7,9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+even.extend(odd)
+print(even)
 
-print(len(even))
-print(len(odd))
 
-print("mississippi".count("issi"))
+even.sort(reverse=True)
+print(even)
+
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+#
+# print(len(even))
+# print(len(odd))
+#
+# print("mississippi".count("issi"))
