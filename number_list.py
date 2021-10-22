4dc77b81c563d61469f4a36c2edd6dcc989cb412
Leffinqwell <S.Bazant@seznam.cz>
2021-10-22 17:03:12 +0200

    PyCharm Commit

--- number_list.py
+++ number_list.py
@@ -2,24 +2,31 @@ empty_list = []
 even = [2,4,6,8]
 odd = [1,3,5,7,9]
 
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
+for number_list in numbers:
+    print(number_list)
 
-digits = sorted("432985617")
-digits_list = list("432985617")
-print(digits)
-print(digits_list)
+    for number in number_list:
+        print(number)
 
-#more_numbers = list(numbers)
-#more_numbers = numbers[:]
-more_numbers = numbers.copy()
-print(more_numbers)
 
-print(numbers is more_numbers)
-print(numbers == more_numbers)
+# sorted_numbers = sorted(numbers)
+# print(sorted_numbers)
+#
+# digits = sorted("432985617")
+# digits_list = list("432985617")
+# print(digits)
+# print(digits_list)
+#
+# #more_numbers = list(numbers)
+# #more_numbers = numbers[:]
+# more_numbers = numbers.copy()
+# print(more_numbers)
+#
+# print(numbers is more_numbers)
+# print(numbers == more_numbers)
 
 # even.extend(odd)
 # print(even)
