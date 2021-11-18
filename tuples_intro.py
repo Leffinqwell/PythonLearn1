0accb68903755ec6cced324966431a8608e87f5f
Leffinqwell <S.Bazant@seznam.cz>
2021-11-18 23:09:28 +0100

    PyCharm Commit

--- tuples_intro.py
+++ tuples_intro.py
@@ -1,16 +1,26 @@
 # t = "a", "b", "c"
 # print(t)
 
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
 
 # print(metallica)
 # print(metallica[0])
 # print(metallica[1])
 # print(metallica[2])
 
-title, artist, year = metallica
-print(title)
+# title, artist, year = metallica
+# print(title)
+# print(artist)
+# print(year)
+
+for album in albums:
+    name, artist, year = album
+    print("album: {}, artist: {}, year: {}".format(name, artist, year))
+
+for name, year, album in albums:
+    print("album: {}, artist: {}, year: {}".format(name, artist, year))
